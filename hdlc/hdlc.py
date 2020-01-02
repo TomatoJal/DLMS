c11736a8b792120877df7dfef9af3efbc797c231
JunFang1 <JunFang1@security.kaifa.cn>
2020-01-02 16:02:04 +0800

    调整flag类写入字段
    Change-Id: I7fdecddbcb3f50ffc6964c5cb8528be825a95b97

--- hdlc/hdlc.py
+++ hdlc/hdlc.py
@@ -15,10 +15,10 @@ class HDLCFlag(DLMSBaseType):
 
     def __init__(self, frame):
         super(HDLCFlag, self).__init__(frame)
-        self.element['flag'] = DLMSBaseType.element_namedtuple(self.frame[0], None)
+        self.element['flag'] = DLMSBaseType.element_namedtuple(self.frame, None)
         # 长度1字节,值恒为0x7E
-        if len(self.frame) == 1 and self.element['flag'].value == self.HDLC_FLAG:
-            f = self.element['flag'].value
+        if len(self.frame) == 1 and self.element['flag'].value[0] == self.HDLC_FLAG:
+            f = self.frame[0]
             self._set_info('flag', 'flag: ' + u8_to_hex(f))
 
 
@@ -36,10 +36,10 @@ class HDLCFormat(DLMSBaseType):
         if len(self.frame) == 2:
             ft = self.frame[0] >> 4
             s = self.frame[0] & 0b00001000
-            l = ((self.frame[0] & 0b00000111) << 8) + self.frame[1]
+            length = ((self.frame[0] & 0b00000111) << 8) + self.frame[1]
             self._set_info('format', 'format type: ' + u8_to_hex(ft) + ',' +
                                      'segmentation: ' + u8_to_hex(s) + ',' +
-                                     'frame length sub-field: ' + u8_to_hex(l))
+                                     'frame length sub-field: ' + u8_to_hex(length))
 
 
 if __name__ == '__main__':
